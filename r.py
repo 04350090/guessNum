b7b04a895980dc34592d96ecfe6a7e4579665c4f
Alan Tai <alan5710804@gmail.com>
2020-10-21 23:57:43 +0800

    Final Version

--- r.py
+++ r.py
@@ -1,12 +1,15 @@
 import random
 # standard library 標準函式庫
 # random is a module
-
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值：')
+end = input('請決定隨機數字範圍開始值：')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 #count = count + 1
-	num = input('請猜1到100的數字：')
+	num = input('請猜數字：')
 	num = int(num)
 	if num == r:
 		print('猜對了！')
