cbdad63edf09ef21b5f37f6f919a294587d23200
Alan Tai <alan5710804@gmail.com>
2020-10-22 00:07:06 +0800

    Try

--- r.py
+++ r.py
@@ -1,10 +1,21 @@
 import random
 # standard library 標準函式庫
 # random is a module
-start = input('請決定隨機數字範圍開始值：')
-end = input('請決定隨機數字範圍開始值：')
-start = int(start)
-end = int(end)
+while True:
+	start = input('請決定隨機數字範圍開始值：')
+	end = input('請決定隨機數字範圍結束值：')
+	if start.isdigit() and end.isdigit():
+		start = int(start)
+		end = int(end)
+		if start >= end:
+			print('開始值不能大於結束值！')
+		elif start == end - 1:
+			print('範圍太小了，請增加') 
+		else:
+			break
+	else:
+		print('請輸入數字！')		
+
 r = random.randint(start, end)
 count = 0
 while True:
