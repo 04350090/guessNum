ed0ff9d6b09f22193357a350e8eac634914e10dc
Alan Tai <alan5710804@gmail.com>
2020-10-21 23:49:05 +0800

    Second Version

--- r.py
+++ r.py
@@ -3,13 +3,17 @@ import random
 # random is a module
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('請猜1到100的數字：')
 	num = int(num)
 	if num == r:
 		print('猜對了！')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('差一點，比答案大')
 	elif num < r:
 		print('差一點，比答案小')
+	print('這是你猜的第', count, '次')
